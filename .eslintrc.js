7bb410c4b6edefe9aad42358382ca152398555c5
lakatostamas <mail.tamaslakatos@gmail.com>
2021-10-25 20:01:39 +0200

    chore: setup code quality & styling tools

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "id-length": 1,
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
